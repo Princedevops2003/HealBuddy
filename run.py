6783140a459ca7be556499f6737147dfb18750a0
PRINCE <pkkhushwaha36@gmail.com>
2026-04-05 16:34:07 +0530

    render fix

--- run.py
+++ run.py
@@ -9,4 +9,8 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=os.environ.get("FLASK_DEBUG", "true").lower() in ("1", "true", "yes"))
+    app.run(
+    host="0.0.0.0",
+    port=int(os.environ.get("PORT", 5000)),
+    debug=os.environ.get("FLASK_DEBUG", "false").lower() in ("1", "true", "yes")
+)
